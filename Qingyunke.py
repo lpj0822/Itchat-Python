77ca837081364768b4f90bd8b1cd812d8cc33d1b
lpj <1014153254@qq.com>
2018-09-24 17:48:08 +0800

    create Itchat

---
+++ Qingyunke.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import requests
+print('你好，我是一枚萌萌哒的机器人！')
+while 1:
+    s=raw_input()
+    resp=requests.get("http://api.qingyunke.com/api.php",{
+        'key':'free',
+        'appid':0,
+        'msg':s
+    })
+    resp.encoding='utf-8'
+    resp=resp.json()
+    print(resp['content'])
